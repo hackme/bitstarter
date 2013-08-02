509ee04595eddcce24eedaaa1f90960599db005e
nitesh <vatsa.nitesh@gmail.com>
2013-08-02 08:23:39 +0000

    reading from another file

--- web.js
+++ web.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
+  var inFile = fs.readFileSync(index.html);
+  console.log(inFile);
   response.send('Hello World2!');
 });
 
